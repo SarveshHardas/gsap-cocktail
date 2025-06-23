cf4b70f5076f04ffa7cdbe8563d77945c32d1522
Sarvesh Hardas <86554291+SarveshHardas@users.noreply.github.com>
2025-06-23 18:02:09 +0530

    implement the about section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import React from 'react'
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
             <Navbar/>
             <Hero/>
             <Cocktails/>
+            <About/>
         </main>
     )
 }
