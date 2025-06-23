dc4c9871985afea5c6f89c03e1e4837b8eff26dc
Sarvesh Hardas <86554291+SarveshHardas@users.noreply.github.com>
2025-06-23 17:01:40 +0530

    implement cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ gsap.registerPlugin(ScrollTrigger, SplitText)
 import React from 'react'
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
 
 const App = () => {
     return (
         <main>
             <Navbar/>
             <Hero/>
+            <Cocktails/>
         </main>
     )
 }
